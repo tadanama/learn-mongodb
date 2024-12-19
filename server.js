79dfd3c8dc52581f27b860d12b13ea9e6432191c
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-19 09:52:47 +0800

    Connect to the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 // Enable environment variables
 dotenv.config();
@@ -8,4 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+	connectDB();
+	console.log(`Server running on port ${port}`);
+});
