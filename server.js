879a800760536863457332b3e5e18baaa9216ff5
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-19 10:43:46 +0800

    Add route to handle create new note

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import Note from "./model/notes.model.js";
 
 // Enable environment variables
 dotenv.config();
@@ -8,6 +9,38 @@ dotenv.config();
 // Instantiate express
 const app = express();
 const port = process.env.PORT;
+
+// Parse json data to req.body
+app.use(express.json());
+
+// Route to create notes
+app.post("/api/notes", async (req, res) => {
+	// Get the new note data
+	const note = req.body;
+	// Destructure the new note data to check if it is empty
+	const { title, text } = note;
+
+	if (!title || !text)
+		return res
+			.status(400)
+			.json({ success: false, message: "All fields required." });
+
+	// Create a new note document
+	const newNote = new Note(note);
+	console.log("note outside trc:", newNote);  // returns the new note data with its _id
+
+	// Save the new note to the database
+	try {
+		await newNote.save();
+		console.log("note inside trc:", newNote);   // returns the new note data with _id + createdAt and updatedAt timestamps
+		res
+			.status(201)
+			.json({ success: true, message: "Successfully create new note" });
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+});
 
 app.listen(port, () => {
 	connectDB();
