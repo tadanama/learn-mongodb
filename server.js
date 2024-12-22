93e65e7d96eba5cd9300dbb8826abf1076c6e4cb
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-22 15:08:10 +0800

    Allow request from other origins with cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import notesRoutes from "./routes/notesRoute.js";
+import cors from "cors";
 
 // Enable environment variables
 dotenv.config();
@@ -12,6 +13,9 @@ const port = process.env.PORT;
 
 // Parse json data to req.body
 app.use(express.json());
+
+// Allow request from different origin
+app.use(cors());
 
 // Notes routes
 app.use("/api/notes", notesRoutes);
